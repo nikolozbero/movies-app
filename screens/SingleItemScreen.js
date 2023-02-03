dc38f4a66bfc462d23e9f52f29501a52331c3c45
Nikoloz Beroshvili <nikolozberoshvili@Nikolozs-MacBook-Pro.local>
2023-02-03 14:06:05 +0400

    sic

--- screens/SingleItemScreen.js
+++ screens/SingleItemScreen.js
@@ -1,4 +1,4 @@
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, StyleSheet, ScrollView} from 'react-native';
 import React from 'react';
 
 const SingleItem = ({route, navigation}) => {
@@ -11,12 +11,30 @@ const SingleItem = ({route, navigation}) => {
   }, [navigation]);
 
   return (
-    <View>
-      <Text>{title}</Text>
-      <Text>{plot}</Text>
-      <Image source={{uri: posterUrl}} />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Image style={styles.image} source={{uri: posterUrl}} />
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.plot}>{plot}</Text>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  title: {fontSize: 22, textAlign: 'center', marginVertical: 12},
+  plot: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  image: {
+    width: '100%',
+    height: 500,
+  },
+});
 
 export default SingleItem;
