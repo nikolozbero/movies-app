4f1bf8a3a8658a4032a4950c7b60e0fc6db59cd8
Nikoloz Beroshvili <nikolozberoshvili@Nikolozs-MacBook-Pro.local>
2023-02-03 16:56:31 +0400

    fix

--- components/FavoriteButton.js
+++ components/FavoriteButton.js
@@ -3,22 +3,10 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import {FavoriteContext} from '../context/context';
 import React from 'react';
 
-function FavoriteButton({id, title, posterUrl, plot}) {
-  const {favoriteMovies, setFavoriteMovies} = React.useContext(FavoriteContext);
-
-  const existingObj = favoriteMovies.some(movie => movie.title === title);
-
-  function onPressHandler() {
-    if (!existingObj) {
-      setFavoriteMovies(prev => [...prev, {id, title, posterUrl, plot}]);
-    } else {
-      setFavoriteMovies(state => state.filter(movie => movie.imdbID === id));
-    }
-  }
-
+function FavoriteButton() {
   return (
-    <Pressable onPress={onPressHandler}>
-      <Icon name={existingObj ? 'heart' : 'heart-o'} size={20} color="red" />
+    <Pressable onPress={() => {}}>
+      <Icon name={'heart-o'} size={20} color="red" />
     </Pressable>
   );
 }
